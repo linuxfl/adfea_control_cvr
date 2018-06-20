60ba11d38bf40083d38308b2a5168b3f685decbd
ling.fang <fangl@bayescom.com>
2018-06-20 14:58:33 +0800

    modify

--- shell/get_active_log.sh
+++ shell/get_active_log.sh
@@ -7,6 +7,10 @@ bridge_machine=192.168.26.105
 remote_active_log_dir=/home/work/log2/model/active
 local_active_log_dir=/home/work/data/bridge_log/active
 batch_active_log_file=/home/work/data/bridge_log/batch_active/batch_active_log
+
+[[ ! -e $remote_active_log_dir ]] && mkdir -p $remote_active_log_dir
+[[ ! -e $local_active_log_dir ]] && mkdir -p $local_active_log_dir
+
 active_log_file=active.${file_time_flag}.log
 
 scp work@${bridge_machine}:${remote_active_log_dir}/${active_log_file} ${local_active_log_dir}/${active_log_file}
@@ -25,6 +29,7 @@ click_log_dir=/home/work/data/click_data
 click_log_file=${click_log_dir}/click_${file_time_flag}
 conversion_file=/home/work/data/conversion_data/conversion_log
 batch_click_log=${click_log_dir}/batch_click_log
+[[ ! -e $click_log_dir ]] && mkdir $click_log_dir
 
 cat $click_log_file >> ${batch_click_log}
 
